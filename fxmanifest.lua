23393adf1fa7598bdfd00fbc59ca76d58f24c9ff
Kznn Dev? <97466596+kaahs2@users.noreply.github.com>
2022-04-21 15:41:51 -0300

    commit init

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author ' тяk 益#0001'
+contact 'тяk 益#0001 https://discord.gg/k4sXStqQXD'
+version '1.0.0'
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"client/*.lua"
+}
+
+files {
+	"nui/hex.js",
+	"nui/hex.html",
+	"nui/hex.css"
+}
+
+ui_page {
+	"nui/hex.html"
+}
