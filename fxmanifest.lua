dac86a57efbad4b0452868026ac76124591113db
Kznn Dev? <97466596+kaahs2@users.noreply.github.com>
2022-04-21 15:59:22 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'bodacious'
 game 'gta5'
 
-author ' тяk 益#0001'
-contact 'тяk 益#0001 https://discord.gg/k4sXStqQXD'
+author ' Kah S2#6969'
+contact 'Kah S2#6969'
 version '1.0.0'
 
 client_scripts {
